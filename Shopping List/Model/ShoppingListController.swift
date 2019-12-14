df061926cc8525066957d5c4fb6e07e11da0d160
Pravin Patel <patelpra337@gmail.com>
2019-12-14 13:26:22 -0600

    Create storyboard, organize project, ViewCell, ViewControllers, Model's and update

---
+++ Shopping List/Model/ShoppingListController.swift
@@ -0,0 +1,85 @@
+//
+//  ShoppingListController.swift
+//  Shopping List
+//
+//  Created by patelpra on 12/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    private (set) var shoppingItems: [ShoppingItem] = []
+    
+    private var shoppingFileURL: URL? {
+        let filemanager = FileManager.default
+        guard let documentsDirectory = filemanager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documentsDirectory.appendingPathComponent("shopping.plist")
+    }
+    
+    // MARK: - CRUD Methods
+    
+    func createShoppingItem(withName name: String) {
+        let shoppingItem = ShoppingItem(withName: name)
+        shoppingItems.append(shoppingItem)
+        self.saveToPersistentStore()
+    }
+    
+    func updateHasBeenAdded(for item: ShoppingItem) {
+        guard let index = shoppingItems.firstIndex(of: item) else { return }
+        shoppingItems[index].hasBeenAdded = !shoppingItems[index].hasBeenAdded
+        self.saveToPersistentStore()
+    }
+    
+    func setShoppingItems() {
+        let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+        for name in itemNames {
+            self.createShoppingItem(withName: name)
+        }
+        
+        
+    }
+    
+    // MARK: - Persistence
+    
+    func saveToPersistentStore() {
+        guard let url = self.shoppingFileURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let shoppingItem = try encoder.encode(self.shoppingItems)
+            try shoppingItem.write(to: url)
+        } catch {
+            print("Error saving shoppingItems: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = self.shoppingFileURL,
+            fileManager.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let shoppingItem = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            self.shoppingItems = try decoder.decode([ShoppingItem].self, from: shoppingItem)
+        } catch {
+            print("Error loading shoppingItems: \(error)")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Shopping List/ViewControllers/ShoppingListDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Spencer Curtis on 8/3/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListDetailViewController: UIViewController {
+
+    @IBOutlet weak var shoppingListLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func sendOrderTapped(_ sendOrderTapped: UIButton) {
+        guard let name = self.nameTextField.text, !name.isEmpty,
+            let address = self.addressTextField.text, !address.isEmpty else { return }
+        
+        if self.shoppingListController!.shoppingItemsAdded.count > 0 {
+            self.showAlert(withName: name, withAddress: address)
+        } else {
+            self.showNoItemsAlert()
+        }
+    }
+    
+    private func updateViews() {
+        if let controller = self.shoppingListController {
+            self.shoppingListLabel.text = "You currently have \(controller.shoppingItemsAdded.count) item(s) in your shopping list."
+        }
+    }
+    
+    private func showAlert(withName name: String, withAddress address: String) {
+        let alert = UIAlertController(title: "Delivery for \(name)", message: "Your shopping items will be delivered to \(address) in 15 minutes.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    private func showNoItemsAlert() {
+        let alert = UIAlertController(title: "Uh Oh", message: "You don't have any items in your shopping list. Please go back and add some.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        self.present(alert, animated: true)
+    }
+}
+
